d5f7f34a297b1234b70cc2084e207f7ddc2d27d2
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2020-01-08 07:17:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const div = document.querySelector("#content");
     
+    function showPosition(position) {
+      var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+      var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
+      "+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY";
+
+      document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+    }
+    
     function onSuccess(location) {
         div.innerText = location.coords.latitude + ", " + location.coords.longitude + ", " + location.coords.altitude + ", " + location.coords.heading;
+        showPosition(location);
     }
 
     function onError(error) {
